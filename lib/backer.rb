36ec33efcadc829e65afff3e4060b34f014ebf44
Claire Harmer <claire.f.harmer@gmail.com>
2018-08-30 16:06:04 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer
   attr_accessor :backer
-  @@backed_projects = []
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
   def back_project(project)
     @project = project
-    @@backed_projects << project
+    @backed_projects << project
   end
 end
